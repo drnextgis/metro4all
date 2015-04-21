eaa90775bfe499b9790b3c5c5311bd9b70bd231a
karavanjo <jownkiv@gmail.com>
2015-04-21 10:31:57 +0300

    JsonifyMixin was added to proj

---
+++ metro4all/utils/__init__.py
@@ -0,0 +1,19 @@
+import datetime
+
+
+class JsonifyMixin:
+    def __init__(self):
+        pass
+
+    def as_json_dict(self, **init):
+        d = dict()
+        for c in self.__table__.columns:
+            v = getattr(self, c.name)
+            if isinstance(v, datetime.datetime):
+                v = v.isoformat()
+            d[c.name] = v
+
+        for k, v in init.items():
+            d[k] = v
+
+        return d
