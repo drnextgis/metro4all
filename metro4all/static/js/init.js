fd0e123dfaafb09c626a8336017ad9c2eab088c6
karavanjo <jownkiv@gmail.com>
2015-04-22 17:15:20 +0300

    templates load functionality was implemented

--- metro4all/static/js/init.js
+++ metro4all/static/js/init.js
@@ -3,9 +3,24 @@ window.metroBugs = {};
 metroBugs.view = {};
 metroBugs.viewmodel = {};
 metroBugs.modules = {};
+metroBugs.templates = {
+    state_checkbox: null
+};
 
 $(document).ready(function () {
-    for(var module in metroBugs.modules) {
-        metroBugs.modules[module].init();
+    var templatesDeferreds = [],
+        templateDeferred;
+
+    for (var template in metroBugs.templates) {
+        templateDeferred = $.get(application_root + 'static/js/templates/' + template + '.mustache', function (template) {
+            metroBugs.templates[template] = template;
+        });
+        templatesDeferreds.push(templateDeferred);
     }
+
+    $.when.apply(window, templatesDeferreds).then(function () {
+        for (var module in metroBugs.modules) {
+            metroBugs.modules[module].init();
+        }
+    });
 });
