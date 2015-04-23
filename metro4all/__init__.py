7bfd548964e94425b83f099b0fbe405c64b99c07
karavanjo <jownkiv@gmail.com>
2015-04-23 03:22:54 +0300

    ACL was added to init app, login and layout views was implemented

--- metro4all/__init__.py
+++ metro4all/__init__.py
@@ -4,21 +4,32 @@ from sqlalchemy import engine_from_config
 from .models import (
     DBSession,
     Base,
-    )
+)
+
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+from .security import groupfinder, Root
 
 
 def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
-    config = Configurator(settings=settings)
+    authn_policy = AuthTktAuthenticationPolicy(
+        'sosecret', callback=groupfinder, hashalg='sha512')
+    authz_policy = ACLAuthorizationPolicy()
+    config = Configurator(settings=settings, root_factory=Root)
+    config.set_authentication_policy(authn_policy)
+    config.set_authorization_policy(authz_policy)
     config.include('pyramid_mako')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_static_view('images', settings.get('upload_path'), cache_max_age=3600)
-    config.add_route('home',          '/')
-    config.add_route('reports',       '/reports')
-    config.add_route('reports_list',  '/reports/list')
-    config.add_route('reports_edit',  '/reports/edit/{id}')
+    config.add_route('home', '/')
+    config.add_route('reports', '/reports')
+    config.add_route('reports_list', '/reports/list')
     config.add_route('change_report_state', '/reports/{id}/state/change')
+    config.add_route('edit', '/edit')
+    config.add_route('login', '/login')
+    config.add_route('logout', '/logout')
     config.scan()
     return config.make_wsgi_app()
--- metro4all/views.py
+++ metro4all/views.py
@@ -7,7 +7,20 @@ import os
 import uuid
 
 from pyramid.response import Response
-from pyramid.view import view_config
+
+from pyramid.view import (
+    view_config,
+    forbidden_view_config,
+    HTTPFound
+)
+
+from pyramid.security import (
+    remember, forget
+)
+
+from pyramid.security import authenticated_userid
+
+from .security import USERS
 
 from .models import (
     DBSession,
@@ -16,10 +29,9 @@ from .models import (
     Report,
     ReportPhoto,
     Node
-    )
+)
 
 from sqlalchemy.orm import joinedload
-from sqlalchemy import update
 
 
 @view_config(route_name='home', renderer='home.mako')
@@ -53,13 +65,13 @@ def reports_list(request):
 
     reports_count = session.query(Report).count()
 
-    query = session.query(Report, ReportCategory.translation['name_ru'], City.translation['name_ru'])\
-        .options(joinedload('photos'))\
-        .options(joinedload('node'))\
-        .options(joinedload('node.stations'))\
-        .join(ReportCategory, Report.category == ReportCategory.id)\
-        .join(City, Report.city == City.old_keyname)\
-        .order_by(column_sorted)\
+    query = session.query(Report, ReportCategory.translation['name_ru'], City.translation['name_ru']) \
+        .options(joinedload('photos')) \
+        .options(joinedload('node')) \
+        .options(joinedload('node.stations')) \
+        .join(ReportCategory, Report.category == ReportCategory.id) \
+        .join(City, Report.city == City.old_keyname) \
+        .order_by(column_sorted) \
         .slice(start_index, start_index + page_size)
 
     reports_from_db = query.all()
@@ -104,23 +116,22 @@ def map_report_fields(view_field):
 def upload(path, base64str):
     id = str(uuid.uuid4().hex)
     full_path = os.path.join(path, "%s.jpg" % id)
-    
+
     with open(full_path, 'wb') as img:
         img.write(base64.b64decode(base64str))
-    
+
     return id
 
 
 @view_config(route_name='reports', request_method='POST')
 def create_report(request):
-    
     body = request.json_body
 
     device_lang = body.get("lang_device")
     data_lang = body.get("lang_data")
     schema_x = body.get("coord_x")
     schema_y = body.get("coord_y")
-    report_on = datetime.fromtimestamp(body.get("time")/1000.0)
+    report_on = datetime.fromtimestamp(body.get("time") / 1000.0)
     package_version = body.get("package_version")
     message = body.get("text")
     email = body.get("email")
@@ -131,9 +142,9 @@ def create_report(request):
     photos = body.get("photos")
 
     if node_old_id:
-        node = DBSession.query(Node)\
-            .filter(Node.old_id == node_old_id)\
-            .filter(Node.old_city_keyname == city)\
+        node = DBSession.query(Node) \
+            .filter(Node.old_id == node_old_id) \
+            .filter(Node.old_city_keyname == city) \
             .one()
     else:
         node = None
@@ -173,14 +184,8 @@ def create_report(request):
         content_type=b'application/json')
 
 
-@view_config(route_name='reports_edit', renderer='edit.mako', request_method='GET')
-def reports_edit(request):
-    session = DBSession()
-    report = session.query(Report).filter(Report.id == request.matchdict['id']).one()
-    return {'report': report}
-
-
-@view_config(route_name='change_report_state', renderer='json', request_method='POST')
+@view_config(route_name='change_report_state', renderer='json', request_method='POST',
+             permission='edit')
 def change_report_state(request):
     state = request.POST['state']
     report_id = request.matchdict['id']
@@ -189,7 +194,54 @@ def change_report_state(request):
     report = session.query(Report).filter(Report.id == report_id).one()
     report.fixed = state
     session.commit()
+    auth = 'true' if authenticated_userid(request) else 'false'
     return {
         'id': report.id,
-        'fixed': report.fixed
+        'fixed': report.fixed,
+        'auth': auth
     }
+
+
+@view_config(name='login', renderer='login.mako')
+@forbidden_view_config(renderer='login.mako')
+def login(request):
+    login_url = request.route_url('login')
+    referrer = request.url
+    if referrer == login_url:
+        referrer = '/'
+    came_from = request.params.get('came_from', referrer)
+    message = ''
+    login_params = ''
+    password = ''
+    if ('login' in request.params) and ('password' in request.params):
+        login_params = request.params['login']
+        password = request.params['password']
+        if USERS.get(login_params) == password:
+            headers = remember(request, login_params)
+            return HTTPFound(location=came_from,
+                             headers=headers)
+        message = 'Failed login'
+
+    return dict(
+        message=message,
+        url=request.application_url + '/login',
+        came_from=came_from,
+        login=login_params,
+        password=password
+    )
+
+
+@view_config(route_name='edit', permission='edit')
+def edit(request):
+    return HTTPFound(location=request.route_url('home'))
+
+
+@view_config(route_name='logout', permission='edit', renderer='logout.mako', request_method="GET")
+def logout(request):
+    return {}
+
+
+@view_config(route_name='logout', permission='edit', renderer='logout.mako', request_method="POST")
+def logout_post(request):
+    headers = forget(request)
+    return HTTPFound(location=request.route_url('home'), headers=headers)
